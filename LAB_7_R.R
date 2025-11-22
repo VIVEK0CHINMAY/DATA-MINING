79e51c179fe206085d3683771177eac4c7214747
VIVEK CHINMAY R <160390017+VIVEK0CHINMAY@users.noreply.github.com>
2025-11-22 11:38:53 +0530

    Add files via upload

---
+++ LAB_7_R.R
@@ -0,0 +1,15 @@
+#LOADING THE DATA
+df <- read.csv("D:/fruits.csv")
+head(data)
+#DESCRIPTION OF DATA
+str(df)
+#CLASS DATATYPE
+class(df$color_score)
+#LENGTH OF A CLASS
+length(df$color_score)
+#DIMENSIONS OF DATA
+dim(df)
+ncol(df)
+# CLASS NAMES / COLUMN NAMES
+names(df)
+
